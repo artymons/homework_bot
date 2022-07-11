8b270e96b6888bb846e7ef9b785ba34f4cb38efa
Артемий Винн <hoztov25@ya.ru>
2022-07-11 18:34:30 +0300

    2 правка

--- homework.py
+++ homework.py
@@ -8,6 +8,7 @@ from http import HTTPStatus
 import requests
 import telegram
 from dotenv import load_dotenv
+from requests import RequestException
 
 load_dotenv()
 
@@ -37,71 +38,72 @@ logger.addHandler(handler)
 
 def send_message(bot, message):
     """Отправляет сообщение в телеграм-чат."""
-    return bot.send_message(TELEGRAM_CHAT_ID, message)
+    try:
+        bot.send_message(TELEGRAM_CHAT_ID, message)
+    except telegram.TelegramError:
+        raise telegram.TelegramError
 
 
 def get_api_answer(current_timestamp):
     """Делает запрос к эндпоинту API-сервиса."""
     timestamp = current_timestamp or int(time.time())
     params = {'from_date': timestamp}
-
-    homework_statuses = requests.get(
-        ENDPOINT,
-        headers=HEADERS,
-        params=params
-    )
-    status = homework_statuses.status_code
-    if status == HTTPStatus.OK:
-        try:
-            return homework_statuses.json()
-        except json.decoder.JSONDecodeError:
-            logger.error('ошибка преобразования к типам данных Python')
-    else:
-        logger.error(
-            f'Недоступность эндпоинта {ENDPOINT}. Код ответа API: {status}'
+    try:
+        homework_statuses = requests.get(
+            ENDPOINT,
+            headers=HEADERS,
+            params=params
         )
-        raise AssertionError
-    return homework_statuses.json()
+        status = homework_statuses.status_code
+    except Exception as error:
+        raise RequestException(f'Ошибка эндпоинта {error}.')
+    try:
+        if status == HTTPStatus.OK:
+            return homework_statuses.json()
+        else:
+            raise RequestException(f'Недоступность эндпоинта {ENDPOINT}.'
+                                   f'Код ответа API: {status}'
+                                   )
+    except json.JSONDecodeError:
+        raise json.JSONDecodeError('Сервер вернул невалидный json')
 
 
 def check_response(response):
     """Проверяет ответ API на корректность.
+
     Если ответ API соответствует ожиданиям,
     то функция должна вернуть список домашних работ.
     """
-    if type(response) is not dict:
-        logger.error('Ответ API не является словарем.')
-        raise TypeError
+    if not isinstance(response, dict):
+        raise TypeError('Ответ API не является словарем.')
     if ('current_date' in response) and ('homeworks' in response):
-        if type(response['homeworks']) is not list:
-            logger.error('Ответ API не соответствует типу.')
-            raise TypeError
+        if not isinstance(response['homeworks'], list):
+            raise TypeError('Ответ API не соответствует заданому типу.')
         homeworks = response.get('homeworks')
         return homeworks
     else:
-        logger.error('Ключи словаря не соответствуют ожиданиям.')
-        raise KeyError
+        raise KeyError('В ответе нет нужных значений.')
 
 
 def parse_status(homework):
     """Извлекает из информации о конкретной домашней работе статус этой работы.
+
     В случае успеха, функция возвращает подготовленную для отправки в Telegram
     строку, содержащую один из вердиктов словаря HOMEWORK_STATUSES.
     """
     homework_name = homework.get('homework_name')
     homework_status = homework.get('status')
     if not homework_name:
-        logger.error(f'отсутствует или пустое поле: {homework_name}')
-        raise KeyError
+        raise KeyError(f'отсутствует или пустое поле: {homework_name}')
     if homework_status not in HOMEWORK_STATUSES:
-        logger.error(f'Неизвестный статус: {homework_status}')
-        raise KeyError
-    verdict = HOMEWORK_STATUSES[homework_status]
+        raise KeyError(f'Неизвестный статус: {homework_status}')
+    verdict = HOMEWORK_STATUSES.get(homework_status)
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
 def check_tokens():
     """Проверяет доступность переменных окружения.
+
     Если отсутствует хотя бы одна переменная окружения —
     функция должна вернуть False, иначе — True.
     """
@@ -115,28 +117,30 @@ def main():
         sys.exit()
 
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
-    current_timestamp = int(time.time())
-
-    ...
-
+    current_timestamp = 1620741126
+    prev_message = ""
     while True:
         try:
             response = get_api_answer(current_timestamp)
             homeworks = check_response(response)
-            if homeworks:
+            message = parse_status(homeworks[0])
+            if prev_message != message:
                 message = parse_status(homeworks[0])
+                prev_message = message
                 send_message(bot, message)
+                logger.info(f'Отправлено сообщение: {message}')
             else:
                 logger.debug('новые статусы в ответе отсутствуют')
+            if homeworks == []:
+                logger.debug("Список домашних работ пустой")
             current_timestamp = response.get('current_date')
-            time.sleep(RETRY_TIME)
-
         except Exception as error:
             message = f'Сбой в работе программы: {error}'
             logger.error(message)
-            send_message(bot, message)
-            time.sleep(RETRY_TIME)
-        else:
+            if prev_message != message:
+                prev_message = message
+                send_message(bot, message)
+        finally:
             time.sleep(RETRY_TIME)
 
 
